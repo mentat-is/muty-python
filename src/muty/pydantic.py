12fd91c520f26f19a8b13072b740172e22ed916a
valerino <xoanino@gmail.com>
2025-01-21 22:54:16 +0000

    fixed autogenerate_model_example_by_class

--- src/muty/pydantic.py
+++ src/muty/pydantic.py
@@ -13,6 +13,15 @@ def autogenerate_model_example_by_class(model_class: BaseModel):
         model_class (BaseModel): the model to generate the example for.
     """
     example = {}
+
+    # check if model has a json_schema_extra with examples
+    if model_class.model_config and "json_schema_extra" in model_class.model_config:
+        # take first example if multiple exist
+        if "examples" in model_class.model_config["json_schema_extra"]:
+            example = model_class.model_config["json_schema_extra"]["examples"][0]
+            return example
+
+    # build example from fields (if they have examples)
     for field_name, field in model_class.model_fields.items():
         if field.json_schema_extra and "examples" in field.json_schema_extra:
             # take first example if multiple exist
