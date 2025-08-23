525ffdfd2fb1560b0424e49d28441587660652e2
valerino <xoanino@gmail.com>
2025-08-23 08:37:27 +0000

    dynload: added load_dynamic_module_from_buffer

--- src/muty/dynload.py
+++ src/muty/dynload.py
@@ -8,6 +8,7 @@ import importlib
 import importlib.util
 import os
 import sys
+import muty.string
 from types import ModuleType
 
 from muty.log import MutyLogger
@@ -39,3 +40,25 @@ def load_dynamic_module_from_file(
     spec.loader.exec_module(mod)
     # print('loaded module:%s, module_name=%s' % (mod, module_name))
     return mod
+
+
+def load_dynamic_module_from_buffer(
+    module_name: str, buffer: bytes, add_to_sys_modules: bool = True
+) -> ModuleType:
+    """
+    Load a dynamic module from a buffer.
+
+    Args:
+        module_name (str): The name of the module.
+        buffer (bytes): The buffer containing the module code.
+        add_to_sys_modules (bool, optional): If True (default), add the module to sys.modules
+    Returns:
+        ModuleType: The loaded module.
+    """
+    sp = importlib.util.spec_from_loader(module_name, loader=None)
+    md = importlib.util.module_from_spec(sp)
+    if add_to_sys_modules:
+        sys.modules[sp.name] = md
+
+    exec(buffer, md.__dict__)
+    return md
